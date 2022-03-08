a101d52d4d23424d1a1ffa163459314d75391568
DouglasB19 <55929934+DouglasB19@users.noreply.github.com>
2022-03-07 22:05:17 -0300

    second commit

--- admin.js
+++ admin.js
@@ -1,14 +1,42 @@
+const mongoose = require("mongoose");
+
+const ProjectSchema = new mongoose.Schema({
+    title: {
+        type: String, 
+        required: true,
+    },
+    description: String,
+    compreted: Boolean,
+    created_at: { type: Date, default: Date.now },
+});
+
+const Project = mongoose.model("Project", ProjectSchema);
+
 const AdminBro = require("admin-bro");
 const AdminBroExpress = require("@admin-bro/express");
+const AdminBroMongoose = require("@admin-bro/mongoose");
 
-const adminBro = new AdminBro({
-    databases: [],
+AdminBro.registerAdapter(AdminBroMongoose);
+
+const adminBroOptions = new AdminBro({
+    resources: [Project],
     rootPath: "/admin",
 });
 
-const router = AdminBroExpress.buildRouter(adminBro);
+const router = AdminBroExpress.buildRouter(adminBroOptions);
 
 const express = require("express");
 const server = express();
 
-server.use(adminBro.options.rootPath, router).lister(5500);
+server.use(adminBroOptions.options.rootPath, router);
+
+const run = async () => {
+    await mongoose.connect("mongodb://localhost/adminbroapp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    await server.listen(5500, () => console.log("Server run"));
+};
+
+run();
